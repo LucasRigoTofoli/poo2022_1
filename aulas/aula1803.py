6d4195df3997b349b7bdd3c5be69b2673ac7dcbb
Lucas Rigo Tofoli <lrtofoli296@gmail.com>
2022-03-18 20:01:15 -0300

    aula do dia 18/03

--- aulas/aula1803.py
+++ aulas/aula1803.py
@@ -1,1 +1,31 @@
-print('aula do dia 18/03')
+from cmath import log10
+
+"""
+x = int(input('Informe um número: '))
+
+y = x % 2
+
+if y == 0:
+    print('O número é par!')
+else:
+    print('O número é ímpar!')
+"""
+
+x = float(input('Informe x: '))
+y = float(input('Informe y: '))
+sinal = input('Informe a operação entre x e y: ')
+
+if sinal == '+':
+    result = x + y
+    print('O resultado da + entre', x,'e', y,'é igual a', result)
+elif sinal == '-':
+    result = x-y
+    print('O resultado da - entre', x,'e', y,'é igual a', result)
+elif sinal == '*':
+    result = x*y
+    print('O resultado da * entre', x,'e', y,'é igual a', result)
+    #print('{:.2f}'.format(x))
+    #print(f'{x:.2f}')
+elif sinal == '/':
+    result = x/y
+    print('O resultado da / entre', x,'e', y,'é igual a', result)
