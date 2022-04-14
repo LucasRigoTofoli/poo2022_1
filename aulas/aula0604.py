7769ee6181edbb6440191404bae6cd0eaf308288
Lucas Rigo Tofoli <44378582+LucasRigoTofoli@users.noreply.github.com>
2022-04-13 21:27:24 -0300

    aula do dia 1304

--- aulas/aula0604.py
+++ aulas/aula0604.py
@@ -1,2 +1,25 @@
-print('hello')
-print('teste')
+# aula do dia 13/04
+
+"""
+num = [5, 5, 5, 4, 6]
+
+def media(num):
+	soma = 0
+	result = 0
+	for x in num:
+		soma += x		
+
+	return soma/(len(num))
+
+result = media(num)
+print(result, "\n")
+
+def media2(num):
+	result = sum(num)/len(num)
+	maximo = max(num)
+	minimo = min(num)
+
+	print("\n\n", result, maximo, minimo)
+
+media2(num)
+"""
